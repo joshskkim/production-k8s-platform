d6a1203250cdafa7da443129883425a24915581c
Josh <josh.sk.kim@gmail.com>
2025-08-28 22:43:59 -0700

    fix: deprecated setting

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -6,7 +6,7 @@ terraform {
     key            = "development/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
+    use_lockfile   = true
   }
 }
 
