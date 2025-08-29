b4327e2ff06b87cdd4ea95c4420f894143da15de
Josh <josh.sk.kim@gmail.com>
2025-08-28 23:24:30 -0700

    fix revert

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.19.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.8.0"
-    }
-  }
-}
-
 # Create monitoring namespace
 resource "kubernetes_namespace" "monitoring" {
   provider = kubernetes
